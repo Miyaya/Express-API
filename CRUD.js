682b24dff7343497a2ff79c4111e362164d8c00e
Miya <miya850604@gmail.com>
2019-10-21 16:36:56 +0800

    Add previous version
    Without promisify

--- CRUD.js
+++ CRUD.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-var util = require('util');
 
 var app = express();
 
@@ -14,66 +13,64 @@ var pool = mysql.createPool({
     database: 'membership'
 });
 
-pool.getConnection((err, connection) => {
+pool.getConnection(function(err) {
     if (err) {
         throw err;
     }
-    if (connection) connection.release();
     console.log('connecting success');
-    return;
 });
 
 // get the body data from req(json)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // create a router to handle routes for a set of RestAPI
 var restAPI = express.Router();
 
-// promisify
-pool.query = util.promisify(pool.query);
-
 // read all accounts uri='/'
-restAPI.get('/', (req, res) => { // function(req, res) {
-    pool.query('SELECT * FROM members')
-    .then (result => {
+restAPI.get('/', function(req, res) {
+    //res.json({ message: "hello world" });
+    pool.query('SELECT * FROM members', function(err, result, field){
+        if(err) {
+            throw err;
+        }
         res.json(result);
-    }).catch ((err) => {
-        throw err;
     });
 });
 
 // read an account uri='/:account'
-restAPI.get('/:account', (req, res) => {
-    pool.query('SELECT * FROM members WHERE account = ?',[req.params.account])
-    .then (result => { // have to promisify pool.query first
+restAPI.get('/:account', function(req, res) {
+    pool.query('SELECT * FROM members WHERE account = ?',[req.params.account],
+    function(err, result, field) {
+        if(err) {
+            throw err;
+        }
         res.json(result);
-    }).catch ((err) => {
-        throw err;
     });
 });
 
 // create uri='/'
-restAPI.post('/', (req, res) => {
+restAPI.post('/', function(req, res) {
+    //res.json({ name: req.body.name, message: "success"});
     pool.query('INSERT INTO members VALUES (?, ?, ?, ?, ?, ?)',
-        [req.body.account, req.body.phone, req.body.birthday, req.body.address, 
-            req.body.data_added_time, req.body.last_modified_time])
-        .then (result => {
-            console.log(result.affectedRows + " record(s) updated");
+        [req.body.account, req.body.phone, req.body.birthday, req.body.address,
+            req.body.data_added_time, req.body.last_modified_time],
+        function(err, result, field) {
+            if(err) {
+                throw err;
+            }
+            console.log(result.affectedRows + " record updated");
             res.json({result: 'success'});
-        }).catch ((err) => {
-            res.json({result: 'failed' });
-            throw err;
         });
 
 });
 
 // update uri='/:account'
-restAPI.put('/:account', (req, res) => {
+restAPI.put('/:account', function(req, res) {
+    //res.json({ id:req.params.id, message: "update " + req.params.id });
     var str = 'UPDATE members SET ';
     var list = [];
     var flag = false;
-
     if(req.body.phone !== 'undefined' && req.body.phone) {
         flag = true;
         str += 'phone = ? ';
@@ -81,47 +78,44 @@ restAPI.put('/:account', (req, res) => {
     }
 
     if(req.body.birthday !== 'undefined' && req.body.birthday) {
-        if(flag) str += ', '; 
+        if(flag) str += ', ';
         flag = true;
         str += 'birthday = ? ';
         list.push(req.body.birthday);
     }
 
     if(req.body.address !== 'undefined' && req.body.address) {
-        if(flag) str += ', '; 
+        if(flag) str += ', ';
         flag = true;
         str += 'address = ? ';
         list.push(req.body.address);
     }
-
-    if(flag) str += ', '; 
+    if(flag) str += ', ';
     str += 'last_modified_time = ? WHERE account = ?';
     list.push(req.body.last_modified_time);
     list.push(req.params.account);
-
-    pool.query(str, list)
-    .then (result => {
-        console.log(result.affectedRows + " record(s) updated");
-        res.json({result: 'success' });
-    }).catch ((err) => {
-        res.json({result: 'failed' });
-    });
+    pool.query(str, list, function(err, result, field) {
+        if(err) {
+            throw err;
+        }
+        console.log(result.affectedRows + " record updated");
+        res.json({result: 'success'});
+    })
 });
 
 // delete uri='/:account'
-restAPI.delete('/:account', (req, res) => {
-    pool.query('DELETE FROM members WHERE account = ?', [req.params.account])
-    .then (result => {
-        console.log(result.affectedRows + " record(s) updated");
+restAPI.delete('/:account', function(req, res) {
+    //res.json({ id: req.params.id, message: "byebye " + req.params.id });
+    pool.query('DELETE FROM members WHERE account = ?', [req.params.account],
+    function(err, result, field) {
+        if(err) {
+            throw err;
+        }
+        console.log(result.affectedRows + " record updated");
         res.json({result: 'success'});
-    }).catch (err => {
-        res.json({result: 'failed' });
-        throw err;
     });
 })
 
 // attach the route for restAPI
 app.use('/', restAPI);
-app.listen(3000, function() {
-    console.log('listening on 3000...');
-})
+app.list
